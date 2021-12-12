ccc7ce11e7d4b789b68ab41a10d40a9013a6d826
Simone Chan <zwsimone@gmail.com>
2021-12-12 23:04:39 +0200

    Add item list in dashboard

--- lib/screens/home/item_display.dart
+++ lib/screens/home/item_display.dart
@@ -2,6 +2,7 @@ import 'package:cheapest_item_calculator/models/item.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cheapest_item_calculator/screens/home/item_tile.dart';
 
 class ItemDisplay extends StatefulWidget {
   const ItemDisplay({Key? key}) : super(key: key);
@@ -16,17 +17,20 @@ class _ItemDisplayState extends State<ItemDisplay> {
 
     final items = Provider.of<List<Item>>(context);
 
-    items.forEach((item) {
-      print(item.category);
-      print(item.name);
-      print(item.units);
-      print(item.uom);
-      print(item.price);
-      print(item.priceperuom);
-    });
+    // items.forEach((item) {
+    //   print(item.category);
+    //   print(item.name);
+    //   print(item.units);
+    //   print(item.uom);
+    //   print(item.price);
+    //   print(item.priceperuom);
+    // });
 
-    return Container(
-      child: Text('list goes here'),
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ItemTile(item: items[index]);
+      },
     );
   }
 }
---
+++ lib/screens/home/item_tile.dart
@@ -0,0 +1,24 @@
+import 'package:cheapest_item_calculator/models/item.dart';
+import 'package:flutter/material.dart';
+
+class ItemTile extends StatelessWidget {
+
+  final Item item;
+
+  const ItemTile({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    String priceValue = (item.priceperuom).toStringAsFixed(2);
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: Text('R $priceValue / ${item.uom}'),
+          title: Text(item.name),
+        ),
+      ),
+    );
+  }
+}
